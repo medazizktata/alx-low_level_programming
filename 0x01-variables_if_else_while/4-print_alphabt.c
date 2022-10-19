9016b0cff881923b5ad2c4162b043bf75cfbfdaf
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-19 16:15:43 -0700

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Description: print alphabt in lower case remove e and q
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97 ; i < 123 ; i++)
+	{
+		if (n == 113 || n == 101)
+		{
+			continue
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
