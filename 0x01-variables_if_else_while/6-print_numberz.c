fcd1735176ab45bbcc240a4c14c67b53a471de44
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-19 16:47:22 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print single digit numbers of base 10 using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		putchar(i);
+	}
+	putchar(10)
+	return (0);
+}
