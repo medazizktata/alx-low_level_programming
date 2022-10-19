8cad7b32755e4cb641bc2b57aa4f5de0a1525fb4
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-19 16:27:25 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	While(i / 10 == 0)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
