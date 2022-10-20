ad137ff7fce1c69f7c3fe6a9362ccdc21ef04566
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-19 18:14:16 -0700

    Task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - main block
+ * @c: parameter
+ * Description: check if c is a letter
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if ((c <= 122 && c >= 97) || (c <= 90 && c >= 65))
+		return (1);
+	else
+		return (0);
+}
