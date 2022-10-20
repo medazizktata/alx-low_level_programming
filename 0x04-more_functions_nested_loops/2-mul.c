a3dc91d1ee129035481b23d036dec285bd6dab43
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-20 12:36:21 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - block
+ * Description: Multiply integers
+ * @a: parameter
+ * @b: parameter
+ * Return: integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
