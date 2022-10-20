7be1ed4a2636f518b217eefa94f8f3c1d38d5bf4
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-19 17:37:58 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97 ; i < 123 ; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
