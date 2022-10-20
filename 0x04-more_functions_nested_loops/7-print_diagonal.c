cf3e3a1ba2cf9b8078f60c937d18f53f79203219
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-20 15:57:23 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - block
+ * Description: draws a diagonal
+ *@n: parameter
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 0 ; i <= n ; i++)
+			_putchar('\\');
+		_putchar('\n');
+	}
+}
