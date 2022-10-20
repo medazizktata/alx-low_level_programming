1fc945ea639bc8f367aa6d9cf32144102d6abdc9
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-20 07:56:11 -0700

    Task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - main block
+ * Description: prints 9 multiplication table
+ * Return: array of values
+ */
+void times_table(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			_putchar("%i\, ", i * j);
+		}
+		_putchar('$');
+	}
+}
