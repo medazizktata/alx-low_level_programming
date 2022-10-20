673dc2f64d4d72a5084f31f08cf9fd038ee40eb5
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-19 17:30:38 -0700

    Task 0 fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include "main.h"
 /**
  * main - main block
- *  Description: print putchar
- *  Return: Always 0 (Success)
+ * Description: print putchar
+ * Return: 0
  */
 int main(void)
 {
-	printf("_putchar\n")
-	return (0)
+	_putchar(10)('_');
+	_putchar(10)('p');
+	_putchar(10)('u');
+	_putchar(10)('t');
+	_putchar(10)('c');
+	_putchar(10)('h');
+	_putchar(10)('a');
+	_putchar(10)('r');
+	_putchar(10)('\n');
+	return (0);
 }
