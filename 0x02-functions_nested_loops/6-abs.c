dda672c4b665d413734418fa4abed6846039bd39
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-19 18:43:34 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - main block
+ * @n: parameter
+ * Description: absolute value of integer
+ * Return: absolute value
+ */
+int _abs(int n);
+{
+	int x;
+
+	if (n < 0)
+		x = -n;
+	else if (n > 0)
+		x = n;
+	return (x);
+}
