d1ae692a1f816065d12762a61970f8e5ad331cd4
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-20 08:08:00 -0700

    Task9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,24 @@
  */
 void times_table(void)
 {
-	int i;
-
-	int j;
+	int i, j, k;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		for (j = 0 ; j < 10 ; j++)
 		{
-			_putchar("%i\, ", i * j);
+			k = i * j;
+			_putchar(k);
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('$');
+		_putchar('\n');
 	}
 }
