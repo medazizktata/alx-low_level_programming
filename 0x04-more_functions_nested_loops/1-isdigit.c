f134633acc8884195965924fc4e7f668050ef39d
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-20 12:21:03 -0700

    Task 1 chack 6

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigit - block
  * Description: check digit
- * @c:parameter
+ * @c: parameter
  * Return: 0 or 1
  */
 int _isdigit(int c)
@@ -10,9 +10,7 @@ int _isdigit(int c)
 	int r;
 
 	if ((c >= 48) || (c <= 57))
-	{
 		r = 1;
-	}
 	else
 		r = 0;
 	return (r);
