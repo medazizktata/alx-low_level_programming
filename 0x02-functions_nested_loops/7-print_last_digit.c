1f73cd339aedcb22bedc7424deb4ef22afa36884
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-19 18:50:49 -0700

    Task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - main block
+ *@n: parameter
+ * Description: prints last digit
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+	return (x);
+}
