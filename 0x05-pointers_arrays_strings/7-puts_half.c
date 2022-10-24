3d8cb63728b34cb92abb0f17f4486fab200f5607
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-24 16:33:25 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - block
+ * @str: pointer parameter
+ * Description: prints a string only half
+ * Return: none
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0 ; j < i / 2 ; j++)
+		_putchar(str[j]);
+	_putchar('\n');)
+}
