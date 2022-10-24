175adee437b8c9037894a22aaeb4bba14e3a9261
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-24 16:44:09 -0700

    Task 7 fix 6.0

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,17 @@ void puts_half(char *str)
 {
 	int i = 0;
 
-	int j;
+	int j, h;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	for (j = (i / 2) ; j < i ; j++)
+	if (i % 2 == 0)
+		h = i / 2;
+	if (i % 2 == 1)
+		h = ((i + 1) / 2);
+	for (j = h ; j < i ; j++)
 		_putchar(str[j]);
 	_putchar('\n');
 }
