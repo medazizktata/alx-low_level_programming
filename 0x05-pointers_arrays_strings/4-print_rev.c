dc3da264d659fe500189eb8b551ba7526732734c
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-24 15:51:23 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - block
+ * @s: pointer parameter
+ * Description: prints a string in reverse
+ * Return: none
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1 ; j >= 0 ; j--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
