75f7859aa32e80263b941829e280d0c5ac256966
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-24 15:37:39 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _puts - block
+ * @str: parameter
+ * Description: prints a string
+ * Return: void
+ */
+void _puts(chat *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
