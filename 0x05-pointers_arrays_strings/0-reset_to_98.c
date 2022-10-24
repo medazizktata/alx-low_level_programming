00a178d38c9923dc804448139180f2b7c5a14120
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-24 15:08:58 -0700

    Renamed directory

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - block
+ * @n: parameter
+ * Description: takes a pointer and updates the value
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
