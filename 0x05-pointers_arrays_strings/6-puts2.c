4f9a5ae22292e4c2c7eabbc83eb95471ebbe44b8
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-24 16:20:47 -0700

    Task 6 fix 1.0

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * puts2 - block
  * @str: pointer parameter
@@ -12,7 +13,9 @@ void puts2(char *str)
 	int j;
 
 	While(str[i] != '\0')
+	{
 		i++;
+	}
 	While((j % 2 == 0) && (j < i))
 	{
 		_putchar(str[j]);
