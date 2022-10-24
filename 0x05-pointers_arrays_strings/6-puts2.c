98d7e07e7992d67c239996316293bfac5413c3a9
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-24 16:18:51 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - block
+ * @str: pointer parameter
+ * Description: prints every other character
+ * Return: none
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	int j;
+
+	While(str[i] != '\0')
+		i++;
+	While((j % 2 == 0) && (j < i))
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
