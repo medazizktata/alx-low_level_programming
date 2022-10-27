dc5164395d938d2b6b0e04e482dbb9ca0da5f147
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-10-27 16:07:12 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - block
+ * Description: comparing strings
+ * @s1: parameter1
+ * @s2: parameter2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	int r = 0;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+	}
+}
