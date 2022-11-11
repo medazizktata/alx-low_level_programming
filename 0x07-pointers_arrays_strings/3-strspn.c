dda5d3f8671487b65284e6b090a8246a82161fd6
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-11-11 12:42:08 -0800

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - block
+ * Description: get string length
+ * @s: parameter
+ * @accept: parameter
+ * Return: integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, cmpt;
+
+	for (i = 0, s[i] >= '\0', i++)
+	{
+		for (j = 0, accept[j] > '\0', j++)
+		{
+			if (s[i] == accept[j])
+			{
+				cmpt++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+	}
+	return (cmpt);
+}
