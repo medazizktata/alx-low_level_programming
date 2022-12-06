27274f00f7f63daaa998e27f537bd756cee78e33
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-06 03:26:00 -0800

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - main block
+ * Description: locates a substring
+ * @haystack: parameter1
+ * @needle: parameter2
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	char chaine;
+
+	for (i = 0; haystack[i] > '\0'; i++)
+	{
+		for (j = 0; needle[j] > '\0', j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				chaine += haystack[i];
+				if (chaine == needle)
+				{
+					return (haystack);
+					continue;
+				}
+			}
+		}
+	}
+	return (0);
+}
