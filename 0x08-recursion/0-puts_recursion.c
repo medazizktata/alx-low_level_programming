4ac83e5903199fcfce67c7e0c3a576aa4a97f3a8
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-19 09:58:13 -0800

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts-recursion - main block
+ * Description: Return a char
+ * @s: parameter1
+ * Result: void
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion((s + 1));
+	}
+}
