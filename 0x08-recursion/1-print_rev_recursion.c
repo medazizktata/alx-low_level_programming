2aced45eaba137263bd62c95398386c01438747f
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-26 09:06:40 -0800

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - main block
+ * Description: print a string in reverse
+ * @s: parameter
+ * Result: void
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i; i > 0 ; i--)
+	{
+		printf("%s", s[i]);
+	}
+	printf("\n");
+}
