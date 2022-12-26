5160d473f846fa9ada33b0aeb9410f8a73ea37d5
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-26 09:57:24 -0800

    Task 3 v3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,19 +7,16 @@
  */
 int factorial(int n)
 {
-	int s;
-
 	if (n < 0)
 	{
-		s = -1;
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		s = 0;
+		return (0);
 	}
 	else
 	{
-		s = s * factorial(n - 1);
+		return (n * factorial(n - 1));
 	}
-	return (s);
 }
