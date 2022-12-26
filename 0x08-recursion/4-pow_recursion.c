1c42ebb7c8c5f95ebdebe449363598a6d0b29937
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-26 10:06:12 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - block
+ * Description: return x raised to power of y
+ * @x: parameter1
+ * @y: parameter2
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
