c989d9eeb5ea35f75871d8ab4caef8642a405d93
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-26 13:21:29 -0800

    Task 0 v8

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,14 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 
+	int i = 0;
+	
 	if (size == 0)
 		return (NULL);
 	array = malloc(size * sizeof(c));
 	if (array == NULL)
 		return (NULL);
-	*array = c;
+	for (i; i < size; i++)
+		array[i] = c;
 	return (array);
 }
