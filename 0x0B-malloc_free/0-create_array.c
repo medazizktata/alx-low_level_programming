4bcc12624e0626f113eaa80a0359e985e3494d60
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-26 13:14:39 -0800

    Task 0 v3

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,9 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 		return (NULL);
-	char *array = (char *)malloc(size * sizeof(char));
+	char *array;
 
+	array = (char *)malloc(size * sizeof(char));
 	if (array == NULL)
 		return (NULL);
 	*array = c;
