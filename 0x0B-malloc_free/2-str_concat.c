c686faa706365dc46b900aadac4c0c2f0e72ee84
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-27 01:50:22 -0800

    Task 2 v6

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,40 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * str_concat - block
+ * str_len - block
+ * Description: returns length of string
+ * @s: char type
+ * Return: integer
+ */
+int str_len(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * str_ccn - block
+ * Description: concatenates two strings
+ * @ch: parameter
+ * @sh: parameter
+ * @n: parameter
+ * Return: pointer
+ */
+char *str_ccn(char *ch; char *sh, int n)
+{
+	int chlen, i;
+
+	chlen = str_len(ch);
+	for (i = 0; i < n && ch[i] != '\0'; i++)
+	{
+		ch[chlen + i] = sh[i];
+	}
+	return (ch);
+}
+/**
+ * str_concat - main block
  * Description: two string concatenation
  * @s1: parameter1
  * @s2: parameter2
@@ -9,7 +42,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k, l;
+	int len_s1, len_s2;
 
 	char *ccn;
 
@@ -17,24 +50,16 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			ccn = malloc(sizeof(char) * (i + j + 1));
-		}
-	}
+	len_s1 = str_len(s1);
+	len_s2 = str_len(s2);
+	ccn = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 	if (ccn == NULL)
 	{
 		free(ccn);
 		return (NULL);
 	}
-	for (k = 0; k < i; k++)
-	{
-		ccn[k] = s1[k];
-	}
-	for (l = 0; l < j; l++)
-		ccn[l + i] = s2[l];
+	str_ccn(ccn, s1, s1_len);
+	str_ccn(ccn, s2, s2_len);
+	ccn += '\0';
 	return (ccn);
-	free(ccn);
 }
