de19f80d2e7ac3834de0cc14485e30a1424277de
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2022-12-31 03:40:41 -0800

    task 0 v2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
-struct dog {
+#ifndef _DOG
+#define _DOG
+/**
+ * struct dog - struct that stors dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}
+} DOG;
+
+#endif
