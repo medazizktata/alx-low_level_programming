7baf1a6e71cea8b47023813132ba063a47f390df
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2023-01-02 05:24:42 -0800

    Task 0 v2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if ((b[len] != '0') && (b[len] != '1'))
 			return (0);
-		s += (s << 1) | (b[i] - '0');
+		s = (s << 1) | (b[i] - '0');
 	}
 	return (s);
 }
