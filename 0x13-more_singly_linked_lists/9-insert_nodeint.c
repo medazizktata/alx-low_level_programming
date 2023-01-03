b08b3087bb9a5338c07490be632af152529d7388
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2023-01-03 12:23:39 -0800

    Task 9 v3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		*head = (**head).next;
 	}
-	if (i == idx)
+	if (i != idx)
 		return (NULL);
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
