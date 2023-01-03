5caa8981e00c1d5b7c0b2b23abc0ca26b4ca9767
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2023-01-03 06:33:37 -0800

    Task 4 v3

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,12 +5,12 @@
  * @head: parameter
  * Return: void
  */
-void free_list(listt *head)
+void free_list(list_t *head)
 {
 	if (head == NULL)
 		return;
 	if ((*head).next != NULL)
 		free_list((*head).next);
-	free((*head).next);
+	free((*head).str);
 	free(head);
 }
