4cdcd0c64fd5fc636ae08f7376d37ad226494be9
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2023-01-03 08:53:19 -0800

    Task 7 v1

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - block
+ * Description: returns the nth nodeof list
+ * @head: parameter
+ * @index: parameter
+ * Return: index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int i, s;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+		head = (*head).next;
+	if (i != index)
+		return (NULL);
+	s = (*head).n;
+	return (s);
+}
+
+
