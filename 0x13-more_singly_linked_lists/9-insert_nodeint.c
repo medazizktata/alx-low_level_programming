bb10801559f684ca1cb62f8403a184d0d636fee3
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2023-01-03 11:49:50 -0800

    Task 9 v1

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - block
+ * Description: add node at given position
+ * @head: parameter
+ * @idx: index
+ * @n: int
+ * Return: list
+ */
+listin_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *newnode;
+	listint_t *node;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; (head != NULL) && (i < idx); i++)
+	{
+		*head = (**head).next;
+	}
+	if (i == idx)
+		return (NULL);
+	node = (**head).next;
+	(**head).next = newnode;
+	(*newnode).n = n;
+	(*newnode).next = node;
+	return (newnode);
+}
