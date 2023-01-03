8e5ed9710cd01153419901d245621011e416eb7b
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2023-01-03 07:13:18 -0800

    Task 1 v1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * listint_len - block
+ * Description: number of elements
+ * @h: parameter
+ * Return: integer
+ */
+size_t listint_len(const listint_t *h)
+{
+	int s = 0;
+
+	while (h != NULL)
+		s++;
+	return (s);
+}
