1a556ef68849daf8bf1d21c6db6b38f1339597e1
Mohamed Aziz Ktata <medazizlearns@gmail.com>
2023-01-03 07:58:22 -0800

    Task 5 v1

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - block
+ * Description: free list
+ * @head: parameter
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *liberator;
+
+	if (*head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		liberator = *head;
+		*head = (**head).next;
+		free(liberator);
+	}
+}
