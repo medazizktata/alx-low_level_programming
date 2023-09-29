10e25f8f5b6acc9b7193d72202cee0400dce1c83
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-09-29 03:39:21 -0700

    Task 0 v0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include main.h
+/**
+ * _puts_recursion - main block
+ * Desciption: print a string
+ * @s:char
+ * Return: string
+ */
+void _puts_recursion(char *s){
+        if (*s == '\0')
+        {
+		printf('\n');
+		return;
+	}
+	printf(%c, *s)
+	_puts_recursion(s + 1);
+}
