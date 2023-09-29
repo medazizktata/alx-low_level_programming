2260aca71a42df38f2fc4dd4b43e48097c8d31c7
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-09-29 04:02:58 -0700

    Task1 v2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,8 @@
  * Return: void
  */
 void _print_rev_recursion(char *s)
-{	
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
+{
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
