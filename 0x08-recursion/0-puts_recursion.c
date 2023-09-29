4f71edad49fece1fdd5020a0f09e2177d2b9977b
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-09-29 03:47:28 -0700

    Task 0 v2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,20 @@
-#include main.h
+#include <main.h>
 #include <stdio.h>
 /**
  * _puts_recursion - main block
  * Desciption: print a string
- * @s:char
- * Return: string
+ * @s: parameter1
+ * Return: void
  */
-void _puts_recursion(char *s){
-        if (*s == '\0')
-        {
-		printf('\n');
-		return;
+void _puts_recursion (char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
 	}
-	printf(%c, *s)
+	else
+	{
+	_putchar(*s);
 	_puts_recursion(s + 1);
+	}
 }
