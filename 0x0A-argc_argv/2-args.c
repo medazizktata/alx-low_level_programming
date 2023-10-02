e8c4327c6454e13e5e93813e8bb2f16a959fb2f8
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-02 03:06:07 -0700

    Task 2 betty

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,14 @@
  * @argv: arguments array
  * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i=0;
-	while(i < argc){
-		printf("%s\n", *(argv+i));
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", *(argv + i));
 		i++;
 	}
-	return 0;
+	return (0);
 }
