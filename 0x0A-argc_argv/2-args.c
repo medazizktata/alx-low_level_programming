f915528bcd6b5d9090882fa835a43c157fdafb25
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-02 02:57:55 -0700

    Task 2 v1

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - block
+ * Description: print arguments
+ * @argc: arg1
+ * @argv: arguments array
+ * Return: 0
+ */
 int main(int argc, char* argv[])
 {
 	int i=0;
