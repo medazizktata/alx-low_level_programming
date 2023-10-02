74cff66035ad553c3d15ca5cf2a162230bd68837
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-02 03:12:26 -0700

    Task 4 betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char* argv[])
+#include <ctype.h>
+/**
+ * main - add positive numbers
+ * @argc: arg1
+ * @argv: numbers array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
-	int m=0;
-	if (argc==1)
-		printf("%d\n", 0);
-	else 
-	{
-	for (int i=0; i<argc; i++){
-		int n=atoi(argv[i]);
-		if (n<0)
-			m+=n;
+	int i, j;
 
+	int add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
 	}
-		printf("%d\n", m);
-		return 0;
-	}
+	printf("%d\n", add);
+	return (0);
 }
