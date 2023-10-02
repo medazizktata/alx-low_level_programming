4ea669ec1213d076d691f877b6a4169ef7505fe3
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-02 02:53:27 -0700

    Task1v2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char* argv[])
+/**
+ * main - block
+ * Description: print number of arguments
+ * @argc: arg1
+ * @argv: arguments array
+ * Return: 0
+ */
+int main(int argc, char* argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return 0;
