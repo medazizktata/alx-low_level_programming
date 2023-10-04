69ab88c0c20d6c243fe4a9edcf801698ce8a0717
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-04 02:50:53 -0700

    Task 0 v1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,13 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		char *array[] = malloc(size * sizeof(char));
+		char *array = (char*)malloc(size * sizeof(char));
+		if (array == NULL)
+			return NULL;
+		for (unsigned int i = 0; i < size; i++)
+		{
+			array[i] = c
+		}
 		return array;
 	}
 }
