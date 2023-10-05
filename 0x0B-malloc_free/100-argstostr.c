cace85ac83e494202051c97ebc31194592f2e482
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-05 12:33:10 -0700

    Task 100 v4

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,20 +10,38 @@ char *argstostr(int ac, char **av)
 {
 	char *s;
 
-	int i;
+	int i, j, , ia, c;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0)
 		return (NULL);
-	s = calloc(ac, sizeof(char));
 	for (i = 0; i < ac; i++)
 	{
-		s += av[i];
-		*s += '\n';
+		if (av[i] = NULL)
+		{
+			return (NULL);
+		}
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
 	}
+	s = malloc((c + 1) * sizeof(char));
 	if (s == NULL)
 	{
 		free(s);
 		return (NULL);
 	}
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			s[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			s[ia] = av[i][j];
+	}
+	s[ia] = '\0';
 	return (s);
 }
