20117f9eeaff09facbd2cb467fbef2dd6b0e138d
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-10 00:01:14 -0700

    Preprocessors tasks v1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - block
+* Description: prints file name
+* Return: int
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is a README file for 0x0D-preprocessor powered by ALX
