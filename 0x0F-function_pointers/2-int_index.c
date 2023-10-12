922504cf766ea5f2a978cd3dfcecc8552d0811ab
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-11 17:21:39 -0700

    Task 2 v2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, int_index;
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	i = 0;
 	while (i < size && cmp(array[i]) == 0)
-	{
 		i++;
-	}
 	if (i == size)
 		int_index = -1;
 	else
