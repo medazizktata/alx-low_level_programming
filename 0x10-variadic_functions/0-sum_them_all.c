ef6bc70f83f4fcbe3a17de68ca8acc7256e48f3f
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-12 16:14:57 -0700

    Task 0 v2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,17 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, s = 0;
-
-	unsigned int num,
+	unsigned int i;
+       
+	int s = 0;
 	
 	va_list args;
 
 	va_start(args, n);
 
-	for (i = 0; i < n, i++)
+	for (i = 0; i < n; i++)
 	{
-		num = va_arg(args, unsigned int);
-		s += num;
+		s += va_arg(args, int);
 	}
 	va_end(args);
 	return (s);
