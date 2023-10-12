6f676a85b365e5e9ecce48b04516fbff94c47a5f
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-11 17:04:18 -0700

    Task 1 v0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* array_iterator - block
+* Description: executes a function given as a parameter on each element of an array
+* @array: parameter1
+* @size: parameter2
+* @action: parameter3
+* Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		while (size -- > 0)
+		{
+			action(*array);
+			array++;
+		}
+	}
+}
