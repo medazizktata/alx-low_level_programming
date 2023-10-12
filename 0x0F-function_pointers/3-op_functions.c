2f9797b852e5c1d79cf2f42a12088efd17f0b39a
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-11 17:55:01 -0700

    Task 3 v0

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "calc.h"
+/**
+* op_add - 1st block
+* Description: performs operation +
+* @a: parameter1
+* @b: parameter2
+* Return: int
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub -2ndt block
+ * Description: performs operation -
+ * @a: parameter1
+ * @b: parameter2
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - 3rd block
+ * Description: performs operation *
+ * @a: parameter1
+ * @b: parameter2
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - 4th block
+ * Description: performs operation /
+ * @a: parameter1
+ * @b: parameter2
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - 5th block
+ * Description: performs operation mod
+ * @a: parameter1
+ * @b: parameter2
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a mod b);
+}
