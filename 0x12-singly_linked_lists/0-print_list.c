32a0dbd55ff6197f89d4d0bbffc0ad70463a8138
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-19 13:08:28 -0700

    created 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_list - block
+* Description: prints all the elements of a list_t list.
+* @size_t: parameter1
+* @const: parameter2
+* @list_t: parameter3
+* @h: parameter4
+* Return: pointer to size_t
+*/
+size_t print_list(const list_t *h) {
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a README file for 0x12-singly_linked_lists powered by ALX
