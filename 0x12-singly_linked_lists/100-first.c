4277c4fec3f51ea9bff9e46539bda73a124ee161
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-19 15:59:12 -0700

    Task 100 v0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * pre_main_message - function before main
+ * Return: no return
+ */
+
+void __attribute__((constructor)) pre_main_message() 
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
