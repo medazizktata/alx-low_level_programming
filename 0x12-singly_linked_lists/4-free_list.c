0b1b94659e2ee9ef5d6aac8fde3e7b5207c2eea9
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-19 15:53:34 -0700

    Task 4 v0

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* free_list - block
+* Description:  frees a list_t list.
+* @head: parameter
+* Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}	
+}
