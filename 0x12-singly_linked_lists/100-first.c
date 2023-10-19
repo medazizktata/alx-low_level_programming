3344fdbb4c894b21cd8eb8e6b7160008d2993c5d
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-19 16:00:30 -0700

    Task 100 v1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * Return: no return
  */
 
-void __attribute__((constructor)) pre_main_message() 
+void __attribute__((constructor)) pre_main_message()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("I bore my house upon my back!\n");
 }
