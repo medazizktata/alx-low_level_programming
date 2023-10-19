9038b6c55bbc4ae0da7231a52faca8657a786c24
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-19 13:16:57 -0700

    Task 0 v0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,25 @@
 #include "main.h"
 /**
-* print_list - block
-* Description: prints all the elements of a list_t list.
-* @size_t: parameter1
-* @const: parameter2
-* @list_t: parameter3
-* @h: parameter4
-* Return: pointer to size_t
-*/
-size_t print_list(const list_t *h) {
+ * print_list - block
+ * Description: prints all the elements of a list_t list.
+ * @h: parameter
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	int s = 0;
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+		{
+			printf("[%d] %s", h.len, h->str);
+			s++;
+		}
+		h = h->next;
+	}
+	printf("-> %d elements", s);
+
 }
