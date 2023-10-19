839c41468e05fd68ee3a4df52068944b8542ecaf
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-19 15:31:21 -0700

    Task 2 v3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,11 +19,6 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 	for (i = 0, str[i], i++)
 		;
 	new_node->len = i;
