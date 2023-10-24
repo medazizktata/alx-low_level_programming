76f8bb15c4646ff2fd87a07496d8705f24e3aeb9
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-24 11:46:15 -0700

    Task 3 v1

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,11 +15,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	while (*head->next != NULL)
+	while ((*head)->next != NULL)
 	{
-		*head = *head->next;
+		*head = (*head)->next;
 	}
-	*head->next = new;
+	(*head)->next = new;
 	new->next = NULL;
 	return (new);
 
