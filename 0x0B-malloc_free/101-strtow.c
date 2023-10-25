7a3666b0047e53abed2504e498e2c850a058664c
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-10-25 15:11:52 -0700

    Final task

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* strtow - block
+* Description: splits a string into words
+* @str: parameter1
+* Return: pointer to pointer to char
+*/
+char **strtow(char *str)
+{
+	char *array;
+	
+	int i, count;
+
+	if (str == NULL || str = "")
+		return (NULL);
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		lettercount = 0;
+		while (str[i] != " ")
+		{
+			lettercount++;
+			i++;
+		}
+		for (k = 0; k < j; k++)
+		count++;
+		words[count] = word;
+		i++;
+	}
+
+
+}
