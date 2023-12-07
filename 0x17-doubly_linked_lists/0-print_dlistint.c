7bc06f6f6ea82a93a6f4f03a31a6b256b6284866
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-07 13:22:31 -0800

    Task 0v0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_dlistint - block
+* Description: prints all the elements of a dlistint_t list.
+* @size_t: parameter1
+* @const: parameter2
+* @dlistint_t: parameter3
+* @h: parameter4
+* Return: pointer to size_t
+*/
+size_t print_dlistint(const dlistint_t *h) {
+}
