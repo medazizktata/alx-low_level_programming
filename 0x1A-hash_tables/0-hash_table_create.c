8f6028d5a925dc847abc3924afc20ec5e30077ad
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2024-01-23 10:42:22 -0800

    Task 0 v5

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	if (size == 0)
-		return NULL;
+		return (NULL);
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
-		return NULL;
+		return (NULL);
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_s *) * size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
-		return NULL;
+		return (NULL);
 	}
-	return hash_table;
+	return (hash_table);
 }
