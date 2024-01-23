2e06ad6443b911c8e1524b93d1ac885b2e5a79b6
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2024-01-23 10:05:30 -0800

    Created header file for hash tables project"

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* hash_table_create - block
+* Description: creates a hash table
+* @hash_table_t: parameter1
+* @size: parameter2
+* Return: pointer to hash_table_t
+*/
+hash_table_t *hash_table_create(unsigned long int size) {
+}
