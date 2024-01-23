3c19bd7fa37f3b01b73ac4a40b47cac5de1dbd7c
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2024-01-23 10:11:26 -0800

    Task 0 v0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,17 @@
-#include "main.h"
+#include "hash_tables.h"
 /**
 * hash_table_create - block
 * Description: creates a hash table
-* @hash_table_t: parameter1
-* @size: parameter2
-* Return: pointer to hash_table_t
+* @size: parameter
+* Return: hash_table_t
 */
-hash_table_t *hash_table_create(unsigned long int size) {
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	hash_table->size = size;
+	hash_table->array = NULL;
+	if (size == 0)
+		return NULL;
+	else
+		return hash_table;
 }
