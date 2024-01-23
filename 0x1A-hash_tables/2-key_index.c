e4eb78e28b25a2b51aa9d188f32502d0fd073fb2
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2024-01-23 10:59:46 -0800

    Task 2 v0

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+* key_index - block
+* Description: function that gives you the index of a key
+* @key: parameter
+* @size: parameter2
+* Return: int
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int i, index;
+	i = hash_djb2(key);
+	index = i % size;
+	return index;
+}
